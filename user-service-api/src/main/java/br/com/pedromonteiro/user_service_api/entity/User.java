ccf73b5806d56442d59e331c837fa40e586a7aff
Pedro Monteiro <pedromont436@gmail.com>
2024-11-30 14:38:59 -0300

    feat: add user entity

---
+++ user-service-api/src/main/java/br/com/pedromonteiro/user_service_api/entity/User.java
@@ -0,0 +1,27 @@
+package br.com.pedromonteiro.user_service_api.entity;
+
+import java.util.Set;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.With;
+import main.java.models.enums.ProfileEnum;
+
+@With
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document
+public class User {
+    
+    @Id
+    private String id;
+    private String name;
+    private String email;
+    private String password;
+    private Set<ProfileEnum> profile;
+}
