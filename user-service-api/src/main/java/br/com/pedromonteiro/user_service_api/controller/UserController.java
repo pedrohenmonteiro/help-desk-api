1c733fc7cf4274dfc983587317a96935d005f87d
Pedro Monteiro <pedromont436@gmail.com>
2024-12-05 00:18:52 -0300

    feat: add jakarta bean validation

--- user-service-api/src/main/java/br/com/pedromonteiro/user_service_api/controller/UserController.java
+++ user-service-api/src/main/java/br/com/pedromonteiro/user_service_api/controller/UserController.java
@@ -16,6 +16,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import models.responses.UserResponse;
 import models.exceptions.StandardError;
 import models.requests.CreateUserRequest;
@@ -59,5 +60,5 @@ public interface UserController {
         )),
     })
     @PostMapping
-    ResponseEntity<Void> save(@RequestBody final CreateUserRequest createUserRequest);
+    ResponseEntity<Void> save(@Valid @RequestBody final CreateUserRequest createUserRequest);
 }
--- user-service-api/src/main/java/br/com/pedromonteiro/user_service_api/controller/exceptions/ControllerExceptionHandler.java
+++ user-service-api/src/main/java/br/com/pedromonteiro/user_service_api/controller/exceptions/ControllerExceptionHandler.java
@@ -1,15 +1,21 @@
 package br.com.pedromonteiro.user_service_api.controller.exceptions;
 
-import java.time.LocalDateTime;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
-import org.springframework.http.HttpStatus;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import jakarta.servlet.http.HttpServletRequest;
 import models.exceptions.ResourceNotFoundException;
 import models.exceptions.StandardError;
+import models.exceptions.ValidationException;
 
 
 @ControllerAdvice
@@ -17,15 +23,34 @@ public class ControllerExceptionHandler {
  
     
     @ExceptionHandler(ResourceNotFoundException.class)
-    ResponseEntity<?> handleNotFoundException(final ResourceNotFoundException ex, final HttpServletRequest request) {
+    ResponseEntity<StandardError> handleNotFoundException(final ResourceNotFoundException ex, final HttpServletRequest request) {
 
         var err = StandardError.builder()
         .timestamp(LocalDateTime.now())
-        .status(HttpStatus.NOT_FOUND.value())
-        .error(HttpStatus.NOT_FOUND.getReasonPhrase())
+        .status(NOT_FOUND.value())
+        .error(NOT_FOUND.getReasonPhrase())
         .message(ex.getMessage())
         .path(request.getRequestURI())
         .build();
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+        return ResponseEntity.status(NOT_FOUND).body(err);
+    }
+
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    ResponseEntity<ValidationException> handleMethodArgumentNotValidException(final MethodArgumentNotValidException ex, final HttpServletRequest request) {
+
+        var err = ValidationException.builder()
+        .timestamp(LocalDateTime.now())
+        .status(BAD_REQUEST.value())
+        .error("Validation Exception")
+        .message("Exception in validation attributes")
+        .path(request.getRequestURI())
+        .errors(new ArrayList<>())
+        .build();
+
+        for(FieldError fieldError : ex.getBindingResult().getFieldErrors()) {
+            err.addError(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+        return ResponseEntity.status(NOT_FOUND).body(err);
     }
 }
