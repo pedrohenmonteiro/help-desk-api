4ff2d9bb0b02b5358528675a643ed6fb910afecc
Pedro Monteiro <pedromont436@gmail.com>
2025-07-23 21:18:43 -0300

    feat: add order message listener

---
+++ emailservice/src/main/java/br/com/pedromonteiro/emailservice/listener/OrderListener.java
@@ -0,0 +1,28 @@
+package br.com.pedromonteiro.emailservice.listener;
+
+import org.springframework.amqp.rabbit.annotation.Exchange;
+import org.springframework.amqp.rabbit.annotation.Queue;
+import org.springframework.amqp.rabbit.annotation.QueueBinding;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+import jakarta.mail.MessagingException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import models.dtos.OrderCreatedMessage;
+
+@Log4j2
+@Component
+@RequiredArgsConstructor
+public class OrderListener {
+
+
+    @RabbitListener(bindings = @QueueBinding(
+            exchange = @Exchange(value = "helpdesk", type = "topic"),
+            value = @Queue(value = "queue.orders"),
+            key = "rk.orders.create"
+    ))
+    public void listener(final OrderCreatedMessage message) throws MessagingException {
+        log.info("Ordem de serviço recebida: {}", message);
+    }
+}
