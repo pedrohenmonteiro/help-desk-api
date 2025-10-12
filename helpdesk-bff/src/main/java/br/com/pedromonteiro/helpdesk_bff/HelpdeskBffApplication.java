3552ea08be9850e8c56b7f7f9cb189b4d29b628d
Pedro Monteiro <pedromont436@gmail.com>
2025-10-12 02:27:20 -0300

    chore: start helpdesk bff project

---
+++ helpdesk-bff/src/main/java/br/com/pedromonteiro/helpdesk_bff/HelpdeskBffApplication.java
@@ -0,0 +1,13 @@
+package br.com.pedromonteiro.helpdesk_bff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpdeskBffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpdeskBffApplication.class, args);
+	}
+
+}
---
+++ helpdesk-bff/src/test/java/br/com/pedromonteiro/helpdesk_bff/HelpdeskBffApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pedromonteiro.helpdesk_bff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpdeskBffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
